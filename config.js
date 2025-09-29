150fafa773ace02eba49480f3cf000225e5a99e3
shrinidhikatti <chinnu.katti@gmail.com>
2025-09-29 13:24:15 +0530

    Astrology Landing Page

---
+++ config.js
@@ -0,0 +1,39 @@
+// config.js - Replace with your actual values
+const CONFIG = {
+    emailjs: {
+        publicKey: '',
+        serviceId: '',
+        templateId: '',
+        customerTemplateId: ''
+    },
+    googleSheets: {
+        scriptUrl: ''
+    },
+    whatsapp: {
+        token: '',
+        phoneNumberId: ''
+    },
+    razorpay: {
+        keyId: ''
+    }
+};
+
+// For development/testing
+const DEV_CONFIG = {
+    emailjs: {
+        publicKey: 'eK_GyH5mwoblODVyg',
+        serviceId: 'service_ns4jxpq',
+        templateId: 'template_kfcis1k',
+        customerTemplateId: 'template_i1gc69e'
+    },
+    googleSheets: {
+        scriptUrl: 'https://script.google.com/macros/s/AKfycbyx5b0kyBVVzfQwFqYLLl7JN7cc5NXAwf0KePrVCH0LYqLr0RW0T5GvTF-S4JlSTQQiWw/exec'
+    },
+    whatsapp: {
+        token: 'test-token',
+        phoneNumberId: 'test-phone-id'
+    },
+    razorpay: {
+        keyId: 'rzp_test_xxxxxxxxx'
+    }
+};
